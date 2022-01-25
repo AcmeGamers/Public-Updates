5409854bd0548d42d03518ab6ca567a31e208466
Acme Gamers <52010365+AcmeGamers@users.noreply.github.com>
2022-01-25 10:07:46 +0500

    Update mouse-scroll.js

--- code-files/js/templates/mouse-scroll.js
+++ code-files/js/templates/mouse-scroll.js
@@ -1,1 +1,20 @@
+// Code Obtained from https://josefzacek.com/blog/how-to-create-scroll-down-animated-mouse-effect-using-html-and-css.html
+// Thank you so much, without your code example, I wouldn't have figured how to do it with css and html.
+
+
+class movingMouse extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+  <div class="moving-mouse-holder">
+    <div class="mouse">
+      <div class="mouse-button">&nbsp;</div>
+    </div>
+  </div>` 
+  }}
+
+customElements.define("scroll-mouse", movingMouse);
+
 
